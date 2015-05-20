24c6f6384a80838ce7c9cfb20757dce081fbb6a3
Alex Tran <vietnogi@gmail.com>
2015-05-20 11:05:22 -0700

    build

--- dist/scripts/timeline.controller.js
+++ dist/scripts/timeline.controller.js
@@ -3,20 +3,40 @@
   var TimelineController;
 
   TimelineController = function(TimelineService, $stateParams) {
-    var activate, onSuccess, vm;
+    var activate, mapEvents, onSuccess, setStatus, vm;
     vm = this;
-    vm.submittedDate = null;
-    vm.quotedDate = null;
-    vm.coPilotedDate = null;
+    mapEvents = [['submitted', 'submitted'], ['email', 'email-verified'], ['quote', 'quote-created'], ['payment', 'payment-accepted'], ['coPilot', 'copilot-assigned'], ['launched', 'launched'], ['joined', 'challenge-member-registered'], ['submissions', 'challenge-submission'], ['feedback', 'challenge-feedback-provided'], ['checkpoint1', 'checkpoint1'], ['finalists', 'finalists'], ['finalistsSelected', 'challenge-finalists-selected'], ['finalDesign', 'final-design'], ['winner', 'winner'], ['finalFeedback', 'final-feedback'], ['completed', 'completed']];
     activate = function() {
-      var params, workId;
+      var j, len, mapEvent, params, workId;
+      for (j = 0, len = mapEvents.length; j < len; j++) {
+        mapEvent = mapEvents[j];
+        vm[mapEvent[0]] = {
+          passed: false,
+          completed: false
+        };
+      }
       params = workId = $stateParams.workId;
       return TimelineService.getEvents(params, onSuccess);
     };
     onSuccess = function(timeline) {
-      vm.submittedDate = timeline.submittedDate;
-      vm.quotedDate = timeline.quotedDate;
-      return vm.coPilotedDate = timeline.coPilotedDate;
+      return setStatus(timeline);
+    };
+    setStatus = function(timeline) {
+      var i, j, k, len, len1, mapEvent, results;
+      for (j = 0, len = mapEvents.length; j < len; j++) {
+        mapEvent = mapEvents[j];
+        vm[mapEvent[0]].completed = timeline.createdDates[mapEvent[1]] != null;
+      }
+      results = [];
+      for (i = k = 0, len1 = mapEvents.length; k < len1; i = ++k) {
+        mapEvent = mapEvents[i];
+        if (mapEvents[i + 1]) {
+          results.push(vm[mapEvent[0]].passed = vm[mapEvents[i + 1][0]].completed);
+        } else {
+          results.push(void 0);
+        }
+      }
+      return results;
     };
     activate();
     return vm;
--- dist/scripts/timeline.service.js
+++ dist/scripts/timeline.service.js
@@ -1,6 +1,8 @@
 (function() {
   'use strict';
-  var srv;
+  var eventTypes, srv;
+
+  eventTypes = ['copilot-assigned', 'created', 'submitted', 'quote-created', 'email-verified', 'payment-accepted', 'challenge-feedback-provided', 'challenge-submission', 'challenge-member-registered', 'challenge-finalists-selected', 'state-change', 'launched', 'checkpoint1', 'finalists', 'final-design', 'winner', 'final-feedback', 'completed'];
 
   srv = function(TimelineAPIService) {
     var findEvent, getCreatedAt, getEvents;
@@ -8,15 +10,15 @@
       var resource;
       resource = TimelineAPIService.query(params);
       resource.$promise.then(function(response) {
-        var coPilotedDate, quotedDate, submittedDate, timeline;
-        submittedDate = getCreatedAt('submitted', response);
-        quotedDate = getCreatedAt('quote-created', response);
-        coPilotedDate = getCreatedAt('copilot-assigned', response);
+        var createdDates, eventType, i, len, timeline;
+        createdDates = {};
+        for (i = 0, len = eventTypes.length; i < len; i++) {
+          eventType = eventTypes[i];
+          createdDates[eventType] = getCreatedAt(eventType, response);
+        }
         timeline = {
           events: response,
-          submittedDate: submittedDate,
-          quotedDate: quotedDate,
-          coPilotedDate: coPilotedDate
+          createdDates: createdDates
         };
         return typeof onSuccess === "function" ? onSuccess(timeline) : void 0;
       });
